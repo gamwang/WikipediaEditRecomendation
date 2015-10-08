917c810b3b7549b77c0958c1cdfb4dc89e101e27
Jonathan Bai <gamwang@berkeley.edu>
2015-10-08 01:16:23 -0700

    Made it modular

--- scrape/scrape.js
+++ scrape/scrape.js
@@ -4,33 +4,36 @@ var request = require('request');
 var _ = require('underscore');
 var fs = require('fs');
 
-function scrapeUserNames(prefix, cb) { 
-    var url = 'https://en.wikipedia.org/w/api.php?action=query&list=allusers&aufrom=' + prefix + '&aulimit=' + 500 + '&format=json&auwitheditsonly=true';
-    request(url, function (err, resp, body) {
-        cb(err, resp, JSON.parse(body));
-    });    
-}
-
-function scrapeUserContrib(users) {
-    _.each(users, function (user) {
-        var name = user.name;
-        var url = 'https://en.wikipedia.org/w/api.php?action=query&list=usercontribs&ucuser=' + user.name + '&uclimit=500&ucdir=newer&format=json';
+module.exports = {
+    scrapeUserNames: function (prefix, cb) { 
+        var url = 'https://en.wikipedia.org/w/api.php?action=query&list=allusers&aufrom=' + prefix + '&aulimit=' + 500 + '&format=json&auwitheditsonly=true';
         request(url, function (err, resp, body) {
-            if (err) {
-                console.log('error');
-                console.log(url);
-                console.log(err);
-            } else {
-                log.write(body);
-                log.write('\n');
-            }
-        });
-    });
-}
+            cb(err, resp, JSON.parse(body));
+        });    
+    },
 
-function getCategory(title, cb) {
-    var url = 'https://en.wikipedia.org/w/api.php?action=query&generator=categories&titles=' + title + '&prop=info&format=json';
-    request(url, cb);
+    scrapeUserContrib: function (users) {
+        _.each(users, function (user) {
+            var name = user.name;
+            var url = 'https://en.wikipedia.org/w/api.php?action=query&list=usercontribs&ucuser=' + user.name + '&uclimit=500&ucdir=newer&format=json';
+            request(url, function (err, resp, body) {
+                if (err) {
+                    console.log('error');
+                    console.log(url);
+                    console.log(err);
+                } else {
+                    log.write(body);
+                    log.write('\n');
+                }
+            });
+        });
+    },
+
+    getCategory: function (title, cb) {
+        var url = 'https://en.wikipedia.org/w/api.php?action=query&generator=categories&titles=' + title + '&prop=info&format=json';
+        request(url, cb);
+    }
+
 }
 
 var cmdline = process.argv;
@@ -42,12 +45,12 @@ if (cmdline[2] == '-con') {
         for (var i = 0; i < 1; i += 1) {
             prefix += possible.charAt(Math.floor(Math.random() * possible.length));
         }
-        scrapeUserNames(prefix, function (err, resp, body) {
+        module.exports.scrapeUserNames(prefix, function (err, resp, body) {
             scrapeUserContrib(body.query.allusers);
         });
     }
 } else if (cmdline[2] == '-cat') {
-    getCategory(cmdline[4], function (err, resp, body) {
+    module.exports.getCategory(cmdline[4], function (err, resp, body) {
         log.write(body);
     });
 }
