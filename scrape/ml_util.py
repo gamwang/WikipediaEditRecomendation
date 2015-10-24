cff3680c692266f63145d6502aeff5f4965392be
Jonathan Bai <gamwang@berkeley.edu>
2015-10-23 21:12:00 -0700

    Added graphing

--- scrape/ml_util.py
+++ scrape/ml_util.py
@@ -2,6 +2,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.cluster import KMeans
 import simplejson as json
+from matplotlib import pyplot
+import numpy as np
 
 def featurize(data):
     ngram_vectorizer = CountVectorizer(analyzer='word', ngram_range=(2, 2), min_df=1)
@@ -25,7 +27,7 @@ def cluster_n(data, n):
     tfidf_vals = tfidf_weights(f_vals)
     est = get_kmeans_estimater(tfidf_vals, n)
     labels = est.labels_
-    return est, labels
+    return est, labels, tfidf_vals
 
 def main():
     f = open('./articles.json', 'r')
@@ -34,7 +36,7 @@ def main():
     articles = []
     i = 0
     for obj in objs:
-        if i > 1000:
+        if i > 500:
             break
         if obj is None or len(obj.strip()) == 0:
             continue
@@ -48,8 +50,20 @@ def main():
         i += 1
     #data_mapping = ["pageid0", "pageid1", "pageid2", "pageid3", "pageid4"]
     #data = ["James likes red peach. James likes red peach.", "Jon likes that James likes red peach. Jon likes that James likes red peach.", "James likes red peach.", "James likes red peach. Haha", "James likes red peach. No way"]
-    estimater, labels = cluster_n(articles, 50)
+    estimater, labels, feats= cluster_n(articles, 3)
     classification = dict(zip(ids, labels))
+    feats = np.array(feats)
+    div_lbls = [[],[],[]]
+    for i in range(len(feats)):
+        feat = feats[i]
+        div_lbls[labels[i]].append(feat)
+    colors = ['r', 'b', 'g']
+    print div_lbls[0]
+    # need a way to visualize dimension of data better; currently only visualizing feature #0 and 1
+    for i in range(len(div_lbls)):
+        cur = np.array(div_lbls[i])
+        pyplot.scatter(cur[:,0], cur[:,1], c=colors[i])
+    pyplot.show()
     print classification
 
 if __name__ == "__main__":
