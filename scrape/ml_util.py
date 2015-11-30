f549aeb9efa08b075dbea2741efbce29192740a9
Jonathan Bai <gamwang@berkeley.edu>
2015-11-29 20:45:13 -0800

    gotta clean it up l8erz

--- scrape/ml_util.py
+++ scrape/ml_util.py
@@ -2,12 +2,34 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.cluster import KMeans
 import simplejson as json
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 import numpy as np
 import time
 import operator
+import math
 
 THRESH_HOLD = 10
+
+def get_data(count):
+    f = open('./articles.json', 'r')
+    objs = f.read().split('\n')
+    ids = []
+    articles = []
+    i = 0
+    for obj in objs:
+        if i > count:
+            break
+        if obj is None or len(obj.strip()) == 0:
+            continue
+        json_obj = json.loads(obj)
+        page_info = json_obj['pages']
+        info = page_info[page_info.keys()[0]]
+        pid = info['pageid']
+        extract = info['extract']
+        ids.append(pid)
+        articles.append(extract)
+        i += 1
+    return articles, ids
 
 def featurize(data):
     ngram_vectorizer = CountVectorizer(analyzer='word', ngram_range=(2, 2), min_df=1)
@@ -43,52 +65,53 @@ def get_kmeans_estimater(data, n):
     return estimater
 
 def cluster_n(data, n):
-    f_names_t, f_vals_t = featurize(data)
-    f_names, f_vals = filter_features(f_names_t, f_vals_t, THRESH_HOLD)
-    tfidf_vals = tfidf_weights(f_vals)
-    est = get_kmeans_estimater(tfidf_vals, n)
+    est = get_kmeans_estimater(data, n)
     labels = est.labels_
-    return est, labels, tfidf_vals, f_names
+    return est, labels
 
-def main():
-    f = open('./articles.json', 'r')
-    objs = f.read().split('\n')
-    ids = []
-    articles = []
-    i = 0
-    for obj in objs:
-        if i > 1000:
-            break
-        if obj is None or len(obj.strip()) == 0:
-            continue
-        json_obj = json.loads(obj)
-        page_info = json_obj['pages']
-        info = page_info[page_info.keys()[0]]
-        pid = info['pageid']
-        extract = info['extract']
-        ids.append(pid)
-        articles.append(extract)
-        i += 1
-    #data_mapping = ["pageid0", "pageid1", "pageid2", "pageid3", "pageid4"]
-    #data = ["James likes red peach. James likes red peach.", "Jon likes that James likes red peach. Jon likes that James likes red peach.", "James likes red peach.", "James likes red peach. Haha", "James likes red peach. No way"]
-    start = time.time()
-    estimater, labels, feats, feat_names = cluster_n(articles, 4)
-    end = time.time()
-    print end - start
-    classification = dict(zip(ids, labels))
+def get_output(labels, feats):
     feats = np.array(feats)
     div_lbls = [[],[],[],[]]
     for i in range(len(feats)):
         feat = feats[i]
         div_lbls[labels[i]].append(feat)
-    colors = ['r', 'b', 'g', 'y']
-    print feat_names
+    return div_lbls
+
+def main():
+    # Get data
+    articles, ids = get_data(1000) # specify how much you want
+    #data_mapping = ["pageid0", "pageid1", "pageid2", "pageid3", "pageid4"]
+    #data = ["James likes red peach. James likes red peach.", "Jon likes that James likes red peach. Jon likes that James likes red peach.", "James likes red peach.", "James likes red peach. Haha", "James likes red peach. No way"]
+    start = time.time()
+    f_names_t, f_vals_t = featurize(articles)
+    print f_vals_t.shape
+    f_names, f_vals = filter_features(f_names_t, f_vals_t, THRESH_HOLD)
+    X = tfidf_weights(f_vals)
+    print X.shape
+    # DO CLUSTERING HERE
+    estimater, labels =  cluster_n(X, 4)
+    end = time.time()
+    print end - start
+    classification = dict(zip(ids, labels)) 
     # need a way to visualize dimension of data better; currently only visualizing feature #0 and 1
-    for i in range(len(div_lbls)):
-        cur = np.array(div_lbls[i])
-        pyplot.scatter(cur[:,0], cur[:,1], c=colors[i])
-    print(cur.shape)
-    pyplot.show()
+    div_lbls = get_output(labels, X)
+
+    num_feats = len(f_names)
+    num_feats_sqrt = math.ceil(num_feats ** (0.5))
+    plt.figure(1)
+    for j in range(1, num_feats):
+        colors = ['r', 'b', 'g', 'y']
+        cur_placement = num_feats_sqrt * 100 + num_feats_sqrt * 10 + j
+        print cur_placement
+        plt.subplot(cur_placement)
+        for i in range(len(div_lbls)):
+            cur = np.array(div_lbls[i])
+            plt.scatter(cur[:,j-1], cur[:,j], c=colors[i])
+        print f_names[j-1], f_names[j]
+        plt.xlabel('weighted frequencies of ' + f_names[j-1])
+        plt.ylabel('weighted frequencies of ' + f_names[j])
+    plt.show()
+    
     #print classification
 
 if __name__ == "__main__":
