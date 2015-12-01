df2baabba5e7161e052ddd2c4a88e780a59bd6e7
Dennis Zhao <denniszhao@berkeley.edu>
2015-11-30 21:13:03 -0800

    reverts ml_util to master

--- scrape/ml_util.py
+++ scrape/ml_util.py
@@ -7,14 +7,12 @@ import numpy as np
 import time
 import operator
 import math
-import enchant
 from sklearn.decomposition import PCA
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import cdist
 import matplotlib.cm as cm
 
 THRESH_HOLD = 7
-WORD_CHECKER = enchant.Dict("en_US")
 
 def get_data(count):
     f = open('./articles.json', 'r')
@@ -40,9 +38,9 @@ def get_data(count):
 def featurize(data):
     ngram_vectorizer = CountVectorizer(analyzer='word', ngram_range=(1, 3), min_df=1)
     counts = ngram_vectorizer.fit_transform(data)
-    f_names = ngram_vectorizer.get_feature_names()
-    f_vals = counts.toarray().astype(int)
-    return f_names, f_vals
+    f_names = ngram_vectorizer.get_feature_names() 
+    f_vals = counts.toarray().astype(int) 
+    return f_names, f_vals 
 
 def filter_features(f_names, f_vals, thresh):
     keep_list = []
@@ -61,9 +59,9 @@ def filter_features(f_names, f_vals, thresh):
     return out_names, out_vals
 
 def tfidf_weights(f_vals):
-    transformer = TfidfTransformer()
+    transformer = TfidfTransformer() 
     tfidf = transformer.fit_transform(f_vals)
-    return tfidf.toarray()
+    return tfidf.toarray() 
 
 def get_kmeans_estimater(data, n):
     estimater = KMeans(init='k-means++', n_clusters=n, n_init=10)
@@ -86,15 +84,10 @@ def get_output(labels, feats, k):
     return div_lbls
 
 def graph(X):
-    pass
-
-def filter_words(text):
-    words = text.split()
-    filtered_words = [word for word in words if WORD_CHECKER.check(word)]
-    return " ".join(filtered_words)
+    pass    
 
 def plot_elbow_method(X):
-    k_range = range(1,30,3)
+    k_range = range(1,30,3) 
     #k_range = [1, 5, 20, 50, 100, 500, 1000]
     # Percentage of variance explained is the ratio of the between-group variance to the total variance, also known as an F-test.
     k_means_var = [get_kmeans_estimater(X, k) for k in k_range]
@@ -116,28 +109,24 @@ def main():
     articles, ids = get_data(1000) # specify how much you want
     #data_mapping = ["pageid0", "pageid1", "pageid2", "pageid3", "pageid4"]
     #data = ["James likes red peach. James likes red peach.", "Jon likes that James likes red peach. Jon likes that James likes red peach.", "James likes red peach.", "James likes red peach. Haha", "James likes red peach. No way"]
-    filtered_articles = map(filter_words, articles)
-    # f_names_t, f_vals_t = featurize(articles)
-    f_names_t, f_vals_t = featurize(filtered_articles)
+    f_names_t, f_vals_t = featurize(articles)
     print f_vals_t.shape
     f_names, f_vals = filter_features(f_names_t, f_vals_t, THRESH_HOLD)
-    print f_vals
     X = tfidf_weights(f_vals)
 
-    # Choose K using "Elbow method"/ F-Test
-    #
+    # Choose K using "Elbow method"/ F-Test 
+    # 
     #plot_elbow_method(X)
-
+    
     # Number of clusters
     k = 9
 
     # DO CLUSTERING HERE (LEGACY CODE)
     estimater, labels =  cluster_n(X, k)
     end = time.time()
-    classification = dict(zip(ids, labels))
-
+    classification = dict(zip(ids, labels)) 
     div_lbls = get_output(labels, X, k)
-
+ 
     # GRAPHING
     num_feats = len(f_names)
     reduced_X = PCA(n_components=2).fit_transform(X)
@@ -156,7 +145,6 @@ def main():
             marker='x', s=169, linewidths=3,
             color='black', zorder=10)
     plt.show()
-
     """
     num_feats_sqrt = math.ceil(num_feats ** (0.5))
     for j in range(1, num_feats):
@@ -172,6 +160,8 @@ def main():
         plt.ylabel('weighted frequencies of ' + f_names[j])
     plt.show()
     """
+    
+    #print classification
 
 if __name__ == "__main__":
     main()
